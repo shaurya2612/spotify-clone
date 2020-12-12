6d7f065bc713ff0341c864ff05e7feb4dc911869
mac <mac@macs-MacBook-Air.local>
2020-12-13 03:46:41 +0530

    Modified styling

--- components/Album/styles.ts
+++ components/Album/styles.ts
@@ -1,11 +1,12 @@
 import { StyleSheet } from "react-native";
-import { scale } from "react-native-size-matters";
+import { moderateScale, scale } from "react-native-size-matters";
 
 const styles = StyleSheet.create({
   container: {
-    width: scale(200),
+    width: scale(135),
+    margin: moderateScale(10, 0.4)
   },
-  image: { width: "100%", height: scale(200) },
+  image: { width: "100%", height: scale(135) },
   text: {
     color: "grey",
     marginTop: 10,
--- components/AlbumCategory/styles.ts
+++ components/AlbumCategory/styles.ts
@@ -1,8 +1,14 @@
 import { StyleSheet } from "react-native";
+import { moderateScale } from "react-native-size-matters";
 
 const styles = StyleSheet.create({
+    container:{
+        margin: moderateScale(10, 0.4)
+    },
     title:{
-        color:"white"
+        color:"white",
+        fontSize:moderateScale(30, 0.4),
+        fontWeight:"bold",
     }
 })
 

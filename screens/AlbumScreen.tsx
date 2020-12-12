55152b36cb9d399de02e9e76c63fd8f0232b0186
mac <mac@macs-MacBook-Air.local>
2020-12-13 04:19:33 +0530

    added AlbumScreen

---
+++ screens/AlbumScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+export const AlbumScreen = () => {
+    return (
+        <View>
+            <Text style={{color:"white"}}>
+                Hello
+            </Text>
+        </View>
+    )
+}
+
+export default AlbumScreen;

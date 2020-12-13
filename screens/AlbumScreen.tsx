f7d60c6966e8bed488058a424ca9c632ab0c6682
Shaurya Jain <shaurya26.jain2@gmail.com>
2020-12-13 16:31:21 +0530

    AlbumHeader added

--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -1,6 +1,7 @@
 import { useRoute } from "@react-navigation/native";
 import React from "react";
 import { FlatList, Text, View } from "react-native";
+import AlbumHeader from "../components/AlbumHeader";
 import SongListItem from "../components/SongListItem";
 import albumDetails from "../data/albumDetails";
 
@@ -8,11 +9,11 @@ export const AlbumScreen = () => {
   const route = useRoute();
   return (
     <View>
-      <Text style={{ color: "white" }}>Hello</Text>
       <FlatList
         data={albumDetails.songs}
         renderItem={({ item }) => <SongListItem song={item} />}
         keyExtractor={(item) => item.id}
+        ListHeaderComponent={() => <AlbumHeader album={albumDetails} />}
       />
     </View>
   );

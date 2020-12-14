7e69d77d7e011c8675bd6d136ed3fcc802e65db7
Shaurya Jain <shaurya26.jain2@gmail.com>
2020-12-14 21:21:09 +0530

    queried album

--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -1,19 +1,44 @@
 import { useRoute } from "@react-navigation/native";
-import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { API, graphqlOperation } from "aws-amplify";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 import AlbumHeader from "../components/AlbumHeader";
 import SongListItem from "../components/SongListItem";
 import albumDetails from "../data/albumDetails";
 
+import {getAlbum} from '../graphql/queries';
+
 export const AlbumScreen = () => {
   const route = useRoute();
+  const albumId = route.params.id; 
+  const [album, setAlbum] = useState(null)
+
+  const fetchAlbumDetails = async () => {
+    try {
+      const data = await API.graphql(graphqlOperation(getAlbum, {id: albumId}))
+      setAlbum(data.data.getAlbum)
+      console.log(data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchAlbumDetails()
+  }, [albumId])
+
+  if (!album)
+  {
+    return <View style={{justifyContent:"center", alignItems:"center"}}><ActivityIndicator color={"white"} size={"large"}/></View>
+  }
+
   return (
     <View>
       <FlatList
-        data={albumDetails.songs}
+        data={album.songs.items}
         renderItem={({ item }) => <SongListItem song={item} />}
         keyExtractor={(item) => item.id}
-        ListHeaderComponent={() => <AlbumHeader album={albumDetails} />}
+        ListHeaderComponent={() => <AlbumHeader album={album} />}
       />
     </View>
   );
